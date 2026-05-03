188590ed67856540371760339058dffeee87425a
SeaMouse <alanclifford76@gmail.com>
2026-05-03 20:11:42 +0200

    Minor asset clean-up.

--- public/assets/poc_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="poc_tiles" tilewidth="32" tileheight="32" tilecount="800" columns="8" objectalignment="topleft" fillmode="preserve-aspect-fit">
- <image source="poc_tiles.png" width="256" height="3200"/>
-</tileset>
